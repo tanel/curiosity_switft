db4cf9a396d8ec256de07f04ef4ce48137e7977a
Tanel Lebedev <tanel.lebedev@gmail.com>
2025-07-28 15:54:48 +0300

    Add video to project

--- Curiosity/Curiosity/ViewController.swift
+++ Curiosity/Curiosity/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
+    var playerLayer: AVPlayerLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.wantsLayer = true
         view.layer?.backgroundColor = NSColor.black.cgColor
+
+        let videoURL = Bundle.main.url(forResource: "video_forward", withExtension: "mp4")!
+        let player = AVPlayer(url: videoURL)
+        player.actionAtItemEnd = .pause
+        player.play()
+
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.bounds
+        layer.autoresizingMask = [.layerWidthSizable, .layerHeightSizable]
+        view.layer?.addSublayer(layer)
+        playerLayer = layer
     }
 
     override var representedObject: Any? {
