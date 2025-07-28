87652dfc9173d40722c74a34505bc06904d32d12
Tanel Lebedev <tanel.lebedev@gmail.com>
2025-07-28 15:12:41 +0300

    Initialize project

---
+++ Curiosity/Curiosity/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Curiosity
+//
+//  Created by Tanel Lebedev on 28.07.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Curiosity/CuriosityTests/CuriosityTests.swift
@@ -0,0 +1,17 @@
+//
+//  CuriosityTests.swift
+//  CuriosityTests
+//
+//  Created by Tanel Lebedev on 28.07.2025.
+//
+
+import Testing
+@testable import Curiosity
+
+struct CuriosityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
